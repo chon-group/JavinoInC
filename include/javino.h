8017b9dfc9af5fa4601e56cd50ae0b0cccaeb755
Bruno Freitas <bruno.freitas@cefet-rj.br>
2023-09-27 13:34:53 -0300

    Implement javino_init() to create a separate thread for the main receive buffer.

--- include/javino.h
+++ include/javino.h
@@ -4,10 +4,13 @@
 
 #include <stdio.h>
 
-
 #define JAVINO_HEADER_LEN 6
 
-char *javino_get_msg(int port);
+void javino_init(int port);
+
+int avaliable_msg();
+
+char* javino_get_msg();
 
 int javino_send_msg(int port, const char* msg_to_send);
 
--- src/javino.c
+++ src/javino.c
@@ -4,35 +4,49 @@
 #include <stdio.h>
 #include <string.h>
 
-
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
 
+char* recv_buffer[ 256 ];
+
+pthread_mutex_t mutex;
+
+pthread_t thread_id;
+
+int in;
+int out;
+int size;
+
+int exogenous_port;
 
 // Returns a message string
 // Returns NULL on error
 
-char* javino_get_msg(int port)
+void* main_loop(void *port)
 {
     char buffer[ 7 ];
+
+	int local_port = *( (int*)port );
 	
 	char msg_size_str[5];
 	
 	int msg_size;
 
+	int nbytes_read;
 
-	//FILE* fd = fopen(port, "r");
+	while (1) {
 	
-	int nbytes_read = read(port, 
-		buffer, 
-		sizeof(char) * 6) ;
-						
-	if ( nbytes_read != (long int)(6*sizeof(char)) ){
-		fprintf(stderr, "\nError! Couldn't get message header!");
+		nbytes_read = read( local_port , 
+			buffer,  
+			sizeof(char) * 6) ;
+							
+		if ( nbytes_read != (long int)(6*sizeof(char)) ){
+			fprintf(stderr, "\nError! Couldn't get message header!");
 
-        return NULL;
+			return NULL;
 
-	} 
+		} 
 #ifdef __EXTRA_DEBUG_MESSAGES__				
 	else {
 
@@ -42,63 +56,125 @@ char* javino_get_msg(int port)
 	}
 #endif	
 		
-	msg_size_str[0] = '0';		
-	msg_size_str[1] = 'x';		
-	msg_size_str[2] = buffer[4];
-	msg_size_str[3] = buffer[5];
-	msg_size_str[4] = '\0';
+		msg_size_str[0] = '0';		
+		msg_size_str[1] = 'x';		
+		msg_size_str[2] = buffer[4];
+		msg_size_str[3] = buffer[5];
+		msg_size_str[4] = '\0';
 
 #ifdef __EXTRA_DEBUG_MESSAGES__						
-	fprintf(stderr, 
-		"\n(javino_get_msg) Message size (string): %s", 
-		msg_size_str);
+		fprintf(stderr, 
+			"\n(javino_get_msg) Message size (string): %s", 
+			msg_size_str);
 #endif		
 		
-	msg_size = strtol(msg_size_str,
-		NULL,
-		0);
+		msg_size = strtol(msg_size_str,
+			NULL,
+			0);
 
 #ifdef __EXTRA_DEBUG_MESSAGES__						
-	fprintf(stderr, 
-    	"\n(javino_get_msg) Message size (int): %d\n", 
-		msg_size);
+		fprintf(stderr, 
+			"\n(javino_get_msg) Message size (int): %d\n", 
+			msg_size);
 #endif
 			
-	char *msg = (char*)malloc(
-		sizeof(char)* (msg_size + 1) );			
+		char *msg = (char*)malloc(
+			sizeof(char)* (msg_size + 1) );			
 			
 			
-	nbytes_read = read( port, 
-		msg, 
-		sizeof(char) * msg_size );
+		nbytes_read = read( local_port, 
+			msg, 
+			sizeof(char) * msg_size );
 			
-	msg[ msg_size ] = '\0';
+		msg[ msg_size ] = '\0';
+
+
+		pthread_mutex_lock( &mutex );
+
+		if (size < 256){
+
+			recv_buffer[ in % 256 ] = msg;
+			in++;
+			size++;
+
+		} else {
+
+			fprintf(stderr, "\nError! Receive buffer is full!");
+
+		}
+
+		pthread_mutex_unlock( &mutex );	
 						
-	if ( (long unsigned)nbytes_read != msg_size*sizeof(char) ){
+		if ( (long unsigned)nbytes_read != msg_size*sizeof(char) ){
 
-		fprintf(stderr, 
-			"\n(javino_get_msg) Error: Expected %lu bytes read, got %lu",
-			msg_size*sizeof(char),
-			(long unsigned)nbytes_read );
+			fprintf(stderr, 
+				"\n(javino_get_msg) Error: Expected %lu bytes read, got %lu",
+				msg_size*sizeof(char),
+				(long unsigned)nbytes_read );
 
-		free( msg );
+			free( msg );
 
-		return NULL;
-	
-    } else {
-            
-			
-	}
+			return NULL;
+		
+		} else {
+				
+				
+		}
 
 #ifdef __DEBUG__							
-	fprintf(stderr,
-		"\n(javino_get_msg) Message received: %s", 
-		msg);
-#endif						
+		fprintf(stderr,
+			"\n(javino_get_msg) Message received: %s", 
+			msg);
+#endif
+	}			
 	
 	//fclose(fd);
+}
 
-    return msg;
+void javino_init(int port){
+
+	pthread_mutex_init( &mutex, NULL);
+
+	in = out = size = 0;
+
+	for (int i =0; i < 256; i ++ ){
+
+		recv_buffer[ i ] = NULL;
+
+	}
+
+	exogenous_port = port;
+
+	pthread_create( &thread_id,
+		NULL,
+		main_loop,
+		(void*)&exogenous_port);
+
+}
+
+
+char* javino_get_msg(){
+
+	char *msg_ptr;
+
+	pthread_mutex_lock( &mutex );
+
+	if (size > 0){
+		msg_ptr = recv_buffer[ out % 256 ];
+
+		recv_buffer[ out % 256 ] = NULL;
+
+		out++;
+		size--;
+	} else {
+
+		msg_ptr = NULL;
+
+	}
+
+	pthread_mutex_unlock( &mutex );
+
+	return msg_ptr;
 }
 
 
@@ -171,4 +247,24 @@ int javino_send_msg(int port, const char* msg_to_send)
 	//fclose( fd );
  
     return nbytes_written;
+}
+
+
+int avaliable_msg(){
+
+	int has_data = 0;
+
+
+	pthread_mutex_lock( &mutex );
+
+	if (size > 0 ){
+
+		has_data = 1;
+
+	}
+
+	pthread_mutex_unlock( &mutex );
+
+	return has_data;	
+
 }
